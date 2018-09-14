d475b3be6bdf9450e6d36daeda03c871e80448e4
Alex Liu <alexliu@Alexs-MacBook-Pro.local>
2018-09-14 08:12:43 -0400

    merging

--- GreyLegs_linV-liuA.py
+++ GreyLegs_linV-liuA.py
@@ -1,47 +1,46 @@
-#Team GreyLegs -- Vincent Lin, Alex Liu
+#Team GreyLegs -- Alex Liu, Vincent Lin
 #SoftDev pd7
 #K06 -- StI/O: Divine your Destiny!
 #2018-09-13
 
-from csv import reader
-from random import randint
+import csv
+from random import choice
 
-d = {}
-reader = reader(open('occupations.csv', 'r')) #reads the csv file 
-r = 0
-for row in reader: #converts the csv file to a dictionary
-    #print(row)
-    if r != 0: #so the first row isn't included 
-        d[float(row[1])] = row[0] #float to make sure percentages are stored as numbers
-    r += 1
 
-#print(d)
+#instantiates the list to fill with occupations
+occuList = []
 
-d.pop(99.8) #remove the total
+#reads the csv file
+reader = csv.DictReader(open('occupations.csv', 'r'))
 
-#print(d) #testing purposes
+def readList():
+    for line in reader:
+        i = 0
+        while i < (float(line['Percentage'])*10):
+            occuList.append(line['Job Class'])
+            i+= 1
+    #print(line)
+    #removes the last line of the csv file
+    for x in range(0,998):
+        occuList.pop()
 
-keys = d.keys()
+#adds more occupations to the occuList to increase frequencies
+def fillPercent():
+    for line in reader:
+        i = 1
+        while i < (float(line['Percentage'])*10):
+            occuList.append(line['Job Class'])
+            i+= 1
 
-keylist = [] 
-keysum = 0.0
-for x in keys: #creating a list of keys
-    keylist.append(x)
-    keysum += x #finding the sum of the keys
 
-#print(keysum)
-#print(keys) #testing perposes
+readList()
+#print(occuList)
 
-def randOcc():
-    ri = float(randint(0, int(keysum))) #choose a random float
-    for x in keylist:
-        if ri > x:
-            #print('ri') #testing
-            #print(ri)
-            ri -= x #subtract until float becomes less than or equal to a key
-        else:
-            #print('x')
-            #print(x)
-            return d[x] #returns value of first valid key
+#testing
+#import pprint
+#pprint.pprint(occuList)
 
-print(randOcc())
+def randomChooser():
+    print (choice(occuList))
+
+randomChooser()
