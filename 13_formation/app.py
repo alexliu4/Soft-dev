64e356078965a8a843183d14f81c19c7e8c2ec7a
Alex Liu <alexliu@Alexs-MBP.fios-router.home>
2018-10-01 01:22:24 -0400

    GET and POST both work

--- 13_formation/app.py
+++ 13_formation/app.py
@@ -5,16 +5,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def template_tester():
-    return render_template("input.html"
-    )
+    return render_template("input.html")
 
-@app.route("/auth")
+@app.route("/auth", methods = ["POST", "GET"])
 def authenticate():
     print(app)
     print(request)
-    print(request.args['username'])
+
+    dictionary = {}
+
+    if request.method=="GET":
+        dictionary = request.args
+
+    if request.method=="POST":
+        dictionary = request.form
+
+    print(app)
+    print(request)
+    print(request.form['username'])
     print(request.method)
-    return '<b>Greetings</b> <br> <br>' + 'method type:' + (request.method) + '<br>username: ' + request.args['username'] + '<b><br>See you next time!</br>'
+    return render_template("result.html",
+                            foo = "hey",
+                            field1 = "username" ,
+                            key1 = dictionary['username'] ,
+                            field2 = "method",
+                            key2 = (request.method))
+    #return '<b>Greetings</b> <br> <br>' + 'method type:' + (request.method) + '<br>username: ' + request.args['username'] + '<b><br>See you next time!</br>'
 
 if __name__ == "__main__":
     app.debug = True
