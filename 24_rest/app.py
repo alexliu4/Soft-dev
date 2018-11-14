ecf1421f0224e35e19540db921c1a3029f1ffd8a
alexliu4 <aliu9@stuy.edu>
2018-11-14 12:56:06 -0500

    tuning

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -1,19 +1,28 @@
-# RayMond -- Alex Liu, Ray Onishi
+# Alex Liu
 # SoftDev1 pd7
 # K24 -- A RESTful Journey Skyward
-# 2018-11-13 F
+# 2018-11-13
 
-import urllib
 from flask import Flask, render_template
+import urllib.request
+import json
+import ssl
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    v = urllib.request.urlopen("https://api.nasa.gov/planetary/apod?api_key=uoaMLMJCuYOsCiK0kN6sxe8zUxVBwNWmuOdMA30D")
+def home():
+    url = "https://api.nasa.gov/planetary/apod?api_key=xygyAxeGinPW9YBcPBX01p9Aba9lXfhrAh5O5WX5"
+    context = ssl._create_unverified_context()
+    v = urllib.request.urlopen(url, context=context)
     print(v)
-    # response = u.read()
-    # data = json.loads( response )
-    return render_template("template.html")
+
+    response = v.read()
+    print(response)
+
+    data = json.loads( response )
+    print (data)
+
+    return render_template("template.html", pic=data['url'], explanation=data['explanation'])
 
 
 if __name__ == "__main__":
